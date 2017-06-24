283e610c22515d421445bb31908ce91649b02983
Guadoo <rock_jk@msn.com>
2017-06-24 16:27:57 +0800

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Liu Jingkai on 2017/6/24.
+//  Copyright © 2017年 Guadoo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
